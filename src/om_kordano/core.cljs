d490b0c947ce2d648b97c53ce176313466b7b23c
kordano <konrad.kuehne@rocketmail.com>
2015-09-01 17:34:12 +0200

    make async requests to server

--- src/om_kordano/core.cljs
+++ src/om_kordano/core.cljs
@@ -1,10 +1,14 @@
 (ns ^:figwheel-always om-kordano.core
-    (:require [om.core :as om]
-              [om.dom :as dom]
-              [secretary.core :as sec :include-macros true]
-              [goog.events :as events]
-              [goog.history.EventType :as EventType])
-    (:import goog.History))
+    (:require  [cljs.reader :as reader]
+               [goog.events :as events]
+               [om.core :as om]
+               [om.dom :as dom]
+               [secretary.core :as sec :include-macros true]
+               [goog.history.EventType :as HEventType])
+    (:import goog.History
+             [goog.net XhrIo]
+             goog.net.EventType
+             [goog.events EventType]))
 
 
 ;; --- HELPERS ---
@@ -15,26 +19,42 @@
 (sec/set-config! :prefix "#")
 
 (let [history (History.)
-      navigation EventType/NAVIGATE]
+      navigation HEventType/NAVIGATE]
   (goog.events/listen history
                      navigation
                      #(-> % .-token sec/dispatch!))
   (doto history (.setEnabled true)))
 
 (def app-state
-  (atom {:views [{:text "home" :url "#/"}
-                 {:text "articles" :url "#/articles"}
-                 {:text "projects" :url "#/projects"}
-                 {:text "services" :url "#/services"}
-                 {:text "readings" :url "#/readings"}
-                 {:text "about" :url "#/about"}]
+  (atom {:views []
          :welcome "Welcome!"}))
+
+(def ^:private meths
+  {:get "GET"
+   :put "PUT"
+   :post "POST"
+   :delete "DELETE"})
+
+(defn edn-xhr [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (on-complete (reader/read-string (.getResponseText xhr)))))
+    (. xhr
+      (send url (meths method) (when data (pr-str data))
+            #js {"Content-Type" "application/edn"}))))
 
 ;; --- NAV ---
 (defn nav-view 
   "Navigation bar"
   [data owner]
   (reify
+    om/IDidMount
+    (did-mount [_]
+      (edn-xhr
+       {:method :get
+        :url "data/views.edn"
+        :on-complete #(om/transact! data :views (fn [_] %))}))
     om/IRender
     (render
       [this]
